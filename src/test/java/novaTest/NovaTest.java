d191c37bb717ecabcda4ac0e42986bb7778386ef
LucyAm <lusysargsyan97@gmail.com>
2018-07-12 18:15:07 +0400

    Login testing

--- src/test/java/novaTest/NovaTest.java
+++ src/test/java/novaTest/NovaTest.java
@@ -44,5 +44,93 @@ public class NovaTest extends FunctionalTestBase {
         AccountPage accountPage = new AccountPage(driver);
         Assert.assertTrue(accountPage.isElementPresent(registeredAccountPageXpath));
     }
+    /*
+    Logging with empty parameters
+     */
+    @Test
+    public void test2() {
+        HomePage homePage = new HomePage(driver);
+        Assert.assertTrue(homePage.isElementPresent(loginXpath));
+        homePage.startLoging();
+
+        LoginPage loginPage = new LoginPage(driver);
+        Assert.assertTrue(loginPage.isElementPresent(emailXpath));
+        loginPage.condirmLoggin("","");
+
+        AccountPage accountPage = new AccountPage(driver);
+        Assert.assertTrue(accountPage.isElementPresent(registeredAccountPageXpath));
+    }
+
+
+    /*
+    Logging with only one parameter
+     */
+    @Test
+    public void test3() {
+        HomePage homePage = new HomePage(driver);
+        Assert.assertTrue(homePage.isElementPresent(loginXpath));
+        homePage.startLoging();
+
+        LoginPage loginPage = new LoginPage(driver);
+        Assert.assertTrue(loginPage.isElementPresent(emailXpath));
+        loginPage.condirmLoggin("likakaul97@gmail.com","");
+
+        AccountPage accountPage = new AccountPage(driver);
+        Assert.assertTrue(accountPage.isElementPresent(registeredAccountPageXpath));
+    }
+
+    /*
+    Logging with only one parameter
+     */
+    @Test
+    public void test4() {
+        HomePage homePage = new HomePage(driver);
+        Assert.assertTrue(homePage.isElementPresent(loginXpath));
+        homePage.startLoging();
+
+        LoginPage loginPage = new LoginPage(driver);
+        Assert.assertTrue(loginPage.isElementPresent(emailXpath));
+        loginPage.condirmLoggin("","rihanna12");
+
+        AccountPage accountPage = new AccountPage(driver);
+        Assert.assertTrue(accountPage.isElementPresent(registeredAccountPageXpath));
+    }
+
+    /*
+    Logging with large and not valid parameters
+     */
+    @Test
+    public void test5() {
+        HomePage homePage = new HomePage(driver);
+        Assert.assertTrue(homePage.isElementPresent(loginXpath));
+        homePage.startLoging();
+
+        LoginPage loginPage = new LoginPage(driver);
+        Assert.assertTrue(loginPage.isElementPresent(emailXpath));
+        loginPage.condirmLoggin("jslkdjalkjdlakjdlakjdlakjdlk@gmail.com","sldjakdjsaldjsaldjalsdjlasj");
+
+        AccountPage accountPage = new AccountPage(driver);
+        Assert.assertTrue(accountPage.isElementPresent(registeredAccountPageXpath));
+    }
+
+    /*
+    Logging without using '@' in email address
+     */
+    @Test
+    public void test6() {
+        HomePage homePage = new HomePage(driver);
+        Assert.assertTrue(homePage.isElementPresent(loginXpath));
+        homePage.startLoging();
+
+        LoginPage loginPage = new LoginPage(driver);
+        Assert.assertTrue(loginPage.isElementPresent(emailXpath));
+        loginPage.condirmLoggin("likakaul97","rihanna12");
+
+        AccountPage accountPage = new AccountPage(driver);
+        Assert.assertTrue(accountPage.isElementPresent(registeredAccountPageXpath));
+    }
+
+
+
 
 }
